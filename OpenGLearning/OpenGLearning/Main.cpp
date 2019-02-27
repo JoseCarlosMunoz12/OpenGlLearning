f64052e59170adbb50f380d42d16f074182409a8
gmunoz0963 <gmunoz0963@student.elgin.edu>
2019-02-26 20:27:45 -0600

    Added GLEW

--- OpenGLearning/OpenGLearning/Main.cpp
+++ OpenGLearning/OpenGLearning/Main.cpp
@@ -1,6 +1,13 @@
 #include "libs.h"
 int main()
 {
+	int d;
+	glewExperimental = GL_TRUE;
+	if (glewInit != GLEW_OK)
+		std::cout << "BASD" << "\n";
+	else
+		std::cout << "MAN";
+	std::cin >> d;
 
 	return 0;
 }
--- OpenGLearning/OpenGLearning/libs.h
+++ OpenGLearning/OpenGLearning/libs.h
@@ -1,4 +1,5 @@
 #pragma once
 
 #include <iostream>
+#include <glew.h>
 #include <glfw3.h>
