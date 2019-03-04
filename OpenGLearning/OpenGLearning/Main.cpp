853cdac37f6f74ebbad08268ce64bb80c20a3105
gmunoz0963 <gmunoz0963@student.elgin.edu>
2019-03-03 20:54:18 -0600

    Able to get a fuzzy image but not a full picture

--- OpenGLearning/OpenGLearning/Main.cpp
+++ OpenGLearning/OpenGLearning/Main.cpp
@@ -1,16 +1,13 @@
 #include "libs.h"
-
-
 //void 
 Vertex vertices[] =
 {
-	//Position                     //Color                     //TexCoords
-	glm::vec3(-0.5f ,0.5f ,0.0f),   glm::vec3(1.0f,0.0f,0.0f),  glm::vec2(0.f,1.f),
-	glm::vec3(-0.5f ,-0.5f,0.0f),   glm::vec3(0.0f,1.0f,0.0f),  glm::vec2(0.f,0.f),
-	glm::vec3(0.5f ,-.5f ,0.0f),   glm::vec3(0.0f,0.0f,1.0f),  glm::vec2(1.f,0.f),
+	//Position                    //Color                     //TexCoords
+	glm::vec3(-0.5f, 0.5f, 0.0f), glm::vec3(1.0f,0.0f,0.0f),  glm::vec2(0.0f,1.0f),
+	glm::vec3(-0.5f,-0.5f, 0.0f), glm::vec3(0.0f,1.0f,0.0f),  glm::vec2(0.0f,0.0f),
+	glm::vec3( 0.5f,-0.5f, 0.0f), glm::vec3(0.0f,0.0f,1.0f),  glm::vec2(1.0f,0.0f),
 
-	glm::vec3(0.5f,0.5f,0.0f),     glm::vec3(0.0f,1.0f,0.0f),  glm::vec2(0.f,0.f)
-
+	glm::vec3( 0.5f, 0.5f, 0.0f), glm::vec3(0.0f,1.0f,0.0f),  glm::vec2(1.0f,1.0f)
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
@@ -162,8 +159,7 @@ int main()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-
+	
 	//Shader INIT
 	GLuint core_program;
 	if (!loadShaders(core_program))
@@ -203,6 +199,32 @@ int main()
 	glEnableVertexAttribArray(2);
 	//BIND VAO 0
 	glBindVertexArray(0);
+	//Texture INIT
+	int image_width = 0;
+	int image_height = 0;
+	unsigned char* image = SOIL_load_image("Images/Untitled1.png",&image_width,&image_height,NULL,SOIL_LOAD_RGBA);
+
+	GLuint texture0;
+	glGenTextures(1, &texture0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image)
+	{
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,image_width, image_height,0,GL_RGBA,GL_UNSIGNED_BYTE,image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << "\n";
+	}
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
 	// MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -211,15 +233,18 @@ int main()
 		//UPdate--
 		updateInput(window);
 		//DRAW---
-
 		//Clear
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 		//Use a Program
 		glUseProgram(core_program);  
+		//Update uniforms
+		glUniform1i(glGetUniformLocation(core_program,"texture0"),0);
+		//Activate Texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture0);
 		//Bind Vertex Array object
 		glBindVertexArray(VAO);
-
 		//Draw
 		//glDrawArrays(GL_TRIANGLES, 0, nrOfVertices);
 		glDrawElements(GL_TRIANGLES, nrOfIndices, GL_UNSIGNED_INT, 0);
@@ -227,6 +252,10 @@ int main()
 		//End Draw
 		glfwSwapBuffers(window);
 		glFlush();
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 	// END OF PROGRAM
 	glfwDestroyWindow(window);
