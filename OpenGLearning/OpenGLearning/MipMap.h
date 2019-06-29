a2d2581ac77a6f8cf0b3982b1a8aa27cf25dd6a3
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2019-06-29 18:23:05 -0500

    Constructor gets Color info from Image

--- OpenGLearning/OpenGLearning/MipMap.h
+++ OpenGLearning/OpenGLearning/MipMap.h
@@ -10,10 +10,27 @@
 
 class MipMap
 {
+
 private:
 	std::vector<std::vector<glm::vec3>> MipMapHolder;
-	MipMap()
+	int Width;
+	int Height;
+	int NumOfChannels;
+
+	MipMap(const char* filename)
 	{
+		unsigned char* image = SOIL_load_image(filename,&this->Width,&this->Height,&this->NumOfChannels, SOIL_LOAD_RGBA);
+		unsigned int BytePerPixel = this->NumOfChannels;
+		for (size_t ii = 0; ii < this->Width; ii++)
+		{
+			std::vector<glm::vec3> TempVec;
+			for (size_t jj = 0; jj < this->Height; jj++)
+			{
+				glm::vec4 TempVec4;
+				unsigned char* PixelOffset = image + (ii + jj * Height) * BytePerPixel;
+				
+			}
+		}
 
 	}
 
@@ -21,7 +38,7 @@ private:
 	{
 
 	}
-	glm::vec3 ReturnValue(int Xpos, int YPos)
+	glm::vec3 ReturnValue(float XPos, float YPos, float WidthPos)
 	{
 
 	}
