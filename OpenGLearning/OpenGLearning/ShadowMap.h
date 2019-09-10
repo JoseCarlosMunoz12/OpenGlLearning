205e69b7e512dd0f683c011f9f7673e5c7915d3b
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2019-09-09 22:14:07 -0500

    Made Shadow Map FBO class. now to render it and use it

--- OpenGLearning/OpenGLearning/ShadowMap.h
+++ OpenGLearning/OpenGLearning/ShadowMap.h
@@ -5,37 +5,56 @@
 #include <glew.h>
 #include <glfw3.h>
 #include <SOIL2.h>
-class Shadow
+class ShadowMapFBO
 {
 
-	GLuint FramebufferName = 0;
+	GLuint FramebufferName;
 	GLuint depthTexture;
 public:
-	Shadow()
+	ShadowMapFBO()
 	{
-		glGenFramebuffers(1, &FramebufferName);
-		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-		// Depth texture. Slower than a depth buffer, but you can sample it later in your shader		
-		glGenTextures(1, &depthTexture);
-		glBindTexture(GL_TEXTURE_2D, depthTexture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, 1024, 1024, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTexture, 0);
-
-		glDrawBuffer(GL_NONE); // No color buffer is drawn to.
-
-		// Always check that our framebuffer is ok
-		glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE;
-		
+		this->FramebufferName = 0;
+		this->depthTexture = 0;
 	};
-	~Shadow()
+	~ShadowMapFBO()
 	{
-		glDeleteTextures(1,&this->depthTexture);
+		if (this->FramebufferName != 0)
+		{
+			glDeleteFramebuffers(1, &this->FramebufferName);
+		}
+		if (this->depthTexture != 0)
+		{
+			glDeleteFramebuffers(1, &this->depthTexture);
+		}
+	}
+	void Init(unsigned int WindowWidth,unsigned int WindowHeight)
+	{
+		//Create the Frame Buffer
+		glGenFramebuffers(1, &this->FramebufferName);
+		//Create The Depth Buffer
+		glGenTextures(1, &this->depthTexture);
+		glBindTexture(GL_TEXTURE_2D, this->depthTexture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, WindowWidth, WindowHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
+		glBindFramebuffer(GL_FRAMEBUFFER, this->FramebufferName);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, this->depthTexture, 0);
+
+		//Disable Writes to the color buffer
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+	}
+	void BindForWriting()
+	{
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, this->FramebufferName);
+	}
+	void BindForReading(GLenum TextureUnit)
+	{
+		glActiveTexture(TextureUnit);
+		glBindTexture(GL_TEXTURE_2D, this->depthTexture);
 	}
 
 };
