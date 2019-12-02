de9b1ff73d8950396583d31b0ff8130f9ae0b03f
josecarlos munoz <josecarlos.munoz12@outlook.com>
2019-12-02 14:19:04 -0600

    Finsih Frames gette rs an Setters

--- OpenGLearning/OpenGLearning/SkelAnimat.h
+++ OpenGLearning/OpenGLearning/SkelAnimat.h
@@ -4,29 +4,36 @@
 class Frames
 {
 	float TimeStamp;
-	Joints* Joint_Trans;
+	Joints Joint_Trans;
 public:
-	Frames(float InitTimeStamp, Joints* InitJoints)
+	Frames(float InitTimeStamp, Joints InitJoints)
 	{
 		this->TimeStamp = InitTimeStamp;
 		this->Joint_Trans = InitJoints;
 	}
-	Frames(float InitTimeStamp, glm::vec3 InitOffset, glm::vec3 InitRot)
-	{
-		this->TimeStamp;
-		this->Joint_Trans = new Joints{InitOffset,InitRot};
-	}
 	glm::quat ReturnRot()
 	{
-		return this->Joint_Trans->GetRot();
+		return this->Joint_Trans.GetRot();
+	}
+	glm::vec3 GetRot()
+	{
+		return this->Joint_Trans.Rotation;
 	}
 	glm::vec3 GetOffset()
 	{
-		return this->Joint_Trans->Offset;
+		return this->Joint_Trans.Offset;
 	}
 	float GetTimeStamp()
 	{
 		return this->TimeStamp;
+	}
+	void SetRot(glm::vec3 NewRot)
+	{
+		this->Joint_Trans.Rotation = NewRot;
+	}
+	void NewOffset(glm::vec3 NewOffset)
+	{
+		this->Joint_Trans.Offset = NewOffset;
 	}
 
 };
@@ -36,6 +43,14 @@ private:
 	SkelAn* Parent;
 	float AnimatLength;
 	std::vector<Frames*> AnimFrames;
+	float GetTimeRatio(float CurrTime)
+	{
+
+	}
+	glm::quat Interpolate(glm::quat FirstAngle, glm::quat Secondangle, float Ratio)
+	{		
+		return glm::mix(FirstAngle, Secondangle, Ratio);
+	}
 public:
 	SkelAn(SkelAn* InitParent,float InitTimeStamp, glm::vec3 InitOffset, glm::vec3 InitRot)
 	{
