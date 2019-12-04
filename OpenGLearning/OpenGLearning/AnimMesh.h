17d1c10da2ae7c828a148908272a7bb1246a9bfa
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2019-12-03 21:30:09 -0600

    creation of VAO and setting data is done, now adding simple rendering Deletes info when in destructor

--- OpenGLearning/OpenGLearning/AnimMesh.h
+++ OpenGLearning/OpenGLearning/AnimMesh.h
@@ -6,7 +6,8 @@
 #include "Vertex.h"
 #include "Shader.h"
 #include "Texture.h"
-#include "Primitive.h"
+#include "AnimLoader.h"
+
 class AnimMesh
 {
 private:
@@ -34,6 +35,42 @@ private:
 			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
 			glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->nrOfIndices * sizeof(GLuint), this->IndexArray, GL_STATIC_DRAW);
 		}
-
+		//Set Vertex AtrivPoitsers an enable (input assembly)
+		//Position
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(AnimVertex), (GLvoid*)offsetof(AnimVertex, position));
+		glEnableVertexAttribArray(0);
+		//Color
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(AnimVertex), (GLvoid*)offsetof(AnimVertex, color));
+		glEnableVertexAttribArray(1);
+		//TexCoord
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(AnimVertex), (GLvoid*)offsetof(AnimVertex, texcoord));
+		glEnableVertexAttribArray(2);
+		//Normal
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(AnimVertex), (GLvoid*)offsetof(AnimVertex, normal));
+		glEnableVertexAttribArray(3);
+		//MatIds
+		glVertexAttribIPointer(4, 3, GL_INT, sizeof(AnimVertex), (GLvoid*)offsetof(AnimVertex, MatId));
+		glEnableVertexAttribArray(4);
+		//Weights
+		glVertexAttribPointer(5, 3, GL_FLOAT, GL_FALSE, sizeof(AnimVertex), (GLvoid*)offsetof(AnimVertex, Weights));
+		glEnableVertexAttribArray(3);
+		//BIND VAO 0
+		glBindVertexArray(0);
+	}
+public:
+	AnimMesh(AnimInf* NewMesh, std::string NewName)
+	{
+		this->NameOfMesh = NewName;
+	}
+	~AnimMesh()
+	{
+		glDeleteVertexArrays(1, &this->VAO);
+		glDeleteBuffers(1, &this->VBO);
+		if (this->nrOfIndices > 0)
+		{
+			glDeleteBuffers(1, &this->EBO);
+		}
+		delete[] this->AnimVerArray;
+		delete[] this->IndexArray;
 	}
 };
