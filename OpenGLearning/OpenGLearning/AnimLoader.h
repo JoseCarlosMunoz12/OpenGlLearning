44384388cddd98b9d31c7b533e9f1a8b1ccae8a5
josecarlos munoz <josecarlos.munoz12@outlook.com>
2019-12-04 14:15:34 -0600

    can extract fron AnimLoader

--- OpenGLearning/OpenGLearning/AnimLoader.h
+++ OpenGLearning/OpenGLearning/AnimLoader.h
@@ -9,6 +9,12 @@
 class AnimInf
 {
 private:
+	std::vector<AnimVertex> Vertices;
+	std::vector<GLuint> Indices;
+	void Set()
+	{
+
+	}
 
 public:
 	AnimInf()
@@ -19,4 +25,10 @@ public:
 	{
 
 	}
+	inline AnimVertex* GetVertices() { return this->Vertices.data(); };
+	inline GLuint* GetIndices() { return this->Indices.data(); };
+
+	inline const unsigned getNrOfVertices() { return this->Vertices.size(); }
+	inline const unsigned getNrOfIndices() { return this->Indices.size(); }
+
 };
