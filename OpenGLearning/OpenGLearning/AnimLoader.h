d0874bf9952beae94b6cf343232e16b3b514d8fe
josecarlos munoz <josecarlos.munoz12@outlook.com>
2019-12-13 15:02:09 -0600

    basic info of Vertex given. now need to put wieghts

--- OpenGLearning/OpenGLearning/AnimLoader.h
+++ OpenGLearning/OpenGLearning/AnimLoader.h
@@ -12,15 +12,38 @@
 class ColladaLoader
 {
 private:
-	std::vector<AnimVertex> FinalsVer;
+	std::vector<AnimVertex> FinalVer;
 	std::vector<GLuint> FinalInd;
-	void MakeAnimVertex()
+	void MakeAnimVertex(aiMesh* meshes)
 	{
-
+		for (int ii = 0; ii < meshes->mNumVertices; ii++)
+		{
+			AnimVertex NewVertex;
+			//Position
+			NewVertex.position.x = meshes->mVertices[ii].x;
+			NewVertex.position.y = meshes->mVertices[ii].y;
+			NewVertex.position.z = meshes->mVertices[ii].z;
+			//Normals
+			NewVertex.normal.x = meshes->mNormals[ii].x;
+			NewVertex.normal.y = meshes->mNormals[ii].y;
+			NewVertex.normal.z = meshes->mNormals[ii].z;
+			//Color
+			NewVertex.color = glm::vec3(0.f, 1.f, 0.f);
+			//Textures
+			NewVertex.texcoord.x = meshes->mTextureCoords[0][ii].x;
+			NewVertex.texcoord.y = meshes->mTextureCoords[0][ii].y;
+			this->FinalVer.push_back(NewVertex);
+		}
 	}
-	void MakeInd()
+	void MakeInd(aiMesh* meshes)
 	{
-
+		for (int ii = 0; ii < meshes->mNumFaces; ii++)
+		{
+			aiFace Faces = meshes->mFaces[ii];
+			FinalInd.push_back(Faces.mIndices[0]);
+			FinalInd.push_back(Faces.mIndices[1]);
+			FinalInd.push_back(Faces.mIndices[2]);
+		}
 	}
 public:
 	ColladaLoader(const char* FileName)
@@ -29,15 +52,17 @@ public:
 		File += FileName;
 		Assimp::Importer importer;
 		const aiScene* scene = importer.ReadFile(File, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices);
-
+		aiMesh* meshes = scene->mMeshes[0];
+		this->MakeAnimVertex(meshes);
+		this->MakeInd(meshes);
 	}
 	std::vector<AnimVertex> GetVertex()
 	{
-
+		return this->FinalVer;
 	}
 	std::vector<GLuint> GetIndices()
 	{
-
+		return this->FinalInd;
 	}
 };
 
@@ -46,10 +71,6 @@ class AnimInf
 private:
 	std::vector<AnimVertex> Vertices;
 	std::vector<GLuint> Indices;
-	void Set()
-	{
-
-	}
 public:
 	AnimInf(const char* FileName)
 	{
