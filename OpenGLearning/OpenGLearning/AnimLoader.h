94746322c94ad7898fc35394b7e0d03570f67134
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-01-17 23:25:34 -0600

    trying to fix angle taking

--- OpenGLearning/OpenGLearning/AnimLoader.h
+++ OpenGLearning/OpenGLearning/AnimLoader.h
@@ -260,7 +260,8 @@ class CLoader: public AnimInf
 					TempQuat.y / glm::sin(AngleRad),
 					TempQuat.z / glm::sin(AngleRad));
 					AngleRad = AngleRad / glm::pi<float>() * 180.f;
-				}				
+				}
+				std::cout << AngleRad << "\n";
 				QuatParts TempQuats = QuatParts(AngleRad, glm::vec3(VecQuat.x, VecQuat.y, -1* VecQuat.z));
 				Joints TempJoint;  
 				TempJoint.Offset = glm::vec3(1.f);
