ebca95d1516812bebf29d1566cde0d3d87c8d9a9
josecarlos munoz <josecarlos.munoz12@outlook.com>
2020-03-13 13:05:10 -0500

    Rebuid Library

--- OpenGLearning/CoatlPhysicsEngine/src/CoatlPhysicsEngine.h
+++ OpenGLearning/CoatlPhysicsEngine/src/CoatlPhysicsEngine.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Collisions/StaticCollisions.h"
-#include "Collisions/Shapes/ColShapes.h"
+#include "Shapes/ColShapes.h"
 #include "Collisions/CollisionWorld.h"
 
 namespace CPE = CoatlPhysicsEngine;
