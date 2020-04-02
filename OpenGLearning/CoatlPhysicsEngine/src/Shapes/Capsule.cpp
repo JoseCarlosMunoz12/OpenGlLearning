01d6d41e79b61b5c335ba445d3d02b49f6b5a537
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-01 22:17:19 -0500

    adding closest point to detect Capsule

--- OpenGLearning/CoatlPhysicsEngine/src/Shapes/Capsule.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Shapes/Capsule.cpp
@@ -5,7 +5,8 @@ Capsule::Capsule(glm::vec3 Pos, float InitRadius, float InitLength)
 	:ColShapes(Pos)
 {
 	this->Radius = InitRadius;
-	this->Length = InitLength;
+	this->BPos = Pos + glm::vec3(0.f,0.f,InitLength/2);
+	this->APos = Pos - glm::vec3(0.f, 0.f, InitLength / 2);
 }
 Capsule::~Capsule()
 {
@@ -16,7 +17,19 @@ float Capsule::GetRadius()
 	return this->Radius;
 }
 
-float Capsule::GetLength()
+glm::vec3 Capsule::ClosestPoint(glm::vec3 Point)
 {
-	return this->Length;
+	glm::vec3 AB = this->BPos - this->APos;
+	float s = glm::dot(AB, AB);
+	float d = glm::dot((Point - this->APos), AB) / s;
+	if (d < 0.f)
+		d = 0.f;
+	if (d > 1.f)
+		d = 1.f;
+
+	glm::vec3 sed = this->APos + d * AB;
+
+	std::cout << "df";
+
+	return glm::vec3();
 }
--- OpenGLearning/CoatlPhysicsEngine/src/Shapes/Capsule.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Shapes/Capsule.h
@@ -4,11 +4,12 @@ namespace CoatlPhysicsEngine{
 	class Capsule: public ColShapes
 	{
 		float Radius;
-		float Length;
+		glm::vec3 APos;
+		glm::vec3 BPos;
 	public:
 		Capsule(glm::vec3 Pos,float InitRadius,float InitLength);
 		~Capsule();
 		float GetRadius();
-		float GetLength();
+		glm::vec3 ClosestPoint(glm::vec3 Point);
 	};
 }
