bb2a40d21a9a1b20b6ddfb6efc7bdbbec8d2889f
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-02 22:14:44 -0500

    Made Collision manager for the Capsules

---
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/CapsuleColCapsule.cpp
@@ -0,0 +1,11 @@
+#include "CapsuleColCapsule.h"
+using namespace CoatlPhysicsEngine;
+
+bool CapsuleColCapsule::CapsuleCols(Capsule Cap0, Capsule Cap1)
+{
+	return false;
+}
+
+CapsuleColCapsule::CapsuleColCapsule()
+{
+}
