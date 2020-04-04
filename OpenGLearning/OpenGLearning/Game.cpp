0a8704197688a82893fd496af0ece04a961f1fed
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-03 20:44:04 -0500

    Sphere and Capsule collision works well

--- OpenGLearning/OpenGLearning/Game.cpp
+++ OpenGLearning/OpenGLearning/Game.cpp
@@ -222,12 +222,12 @@ void Game::initModels()
 		{ this->textures[10],this->textures[10],
 		this->textures[14],this->textures[15],this->textures[16] },
 		meshes[5], { Monk }));
-	this->models.push_back(new Model("Capsule",
+	this->models.push_back(new Model("Sphere0",
 		glm::vec3(0.f, 0.f, this->MipMapsData[0]->ReturnValue(0.f, 0.f)),
 		this->MatTest[1],
 		{ this->textures[10],this->textures[10],
 		this->textures[14],this->textures[15],this->textures[16] },
-		meshes[5], { Monk }));
+		meshes[1], { Monk }));
 
 	//anim Models
 	//this->animModel.push_back(new AnimModel("Tes1t",
