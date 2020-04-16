0ff1bf81fe886504169d0c66519f2934ddd3ef02
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-15 19:21:40 -0500

    Adding Trianlge and AABB collision

---
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/TriangleColAABB.cpp
@@ -0,0 +1,15 @@
+#include "TriangleColAABB.h"
+using namespace CoatlPhysicsEngine;
+
+bool TriangleColAABB::TrColAABB(Triangles Tr, AABB_Obj AABB)
+{
+	return false;
+}
+
+TriangleColAABB::TriangleColAABB()
+{
+}
+
+TriangleColAABB::~TriangleColAABB()
+{
+}
---
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/TriangleColAABB.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../Shapes/Triangles.h"
+#include "../Shapes/AABB_Obj.h"
+namespace CoatlPhysicsEngine {
+	class TriangleColAABB
+	{
+	protected:
+		bool TrColAABB(Triangles Tr, AABB_Obj AABB);
+	public:
+		TriangleColAABB();
+		~TriangleColAABB();
+	};
+}
