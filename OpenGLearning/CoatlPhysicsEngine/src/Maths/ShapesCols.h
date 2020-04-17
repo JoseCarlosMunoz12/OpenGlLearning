7bb38de26863669545a12ab2b6f909b0f0445a2d
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-16 20:12:12 -0500

    making the math namespace  and other collisions in a seperate .h page

---
+++ OpenGLearning/CoatlPhysicsEngine/src/Maths/ShapesCols.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\type_ptr.hpp>
+#include <vector>
+
+namespace CoatlPhysicsEngine {
+
+}
