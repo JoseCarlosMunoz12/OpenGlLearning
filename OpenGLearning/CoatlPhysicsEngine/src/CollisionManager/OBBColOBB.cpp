bbf0de72101e4b02d3933bc935da9534b4a71ca9
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-20 21:55:08 -0500

    made functions in the OBBColOBB class

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/OBBColOBB.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/OBBColOBB.cpp
@@ -1,2 +1,15 @@
 #include "OBBColOBB.h"
 using namespace CoatlPhysicsEngine;
+
+bool OBBColOBB::OBBCol(OBB Ob0, OBB Ob1)
+{
+	return true;
+}
+
+OBBColOBB::OBBColOBB()
+{
+}
+
+OBBColOBB::~OBBColOBB()
+{
+}
