c47c1b8dd79e9500b1d37c93542478657b0b0924
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-04-23 22:10:12 -0500

    added getting Normal Vectors of Segments

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/OBBColOBB.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionManager/OBBColOBB.cpp
@@ -6,16 +6,20 @@ bool OBBColOBB::OBBCol(OBB Ob0, OBB Ob1)
 	std::vector<glm::vec3> Ob0_Segs = Ob0.GetSegments();
 	std::vector<glm::vec3> Ob1_Segs = Ob1.GetSegments();
 
-	std::vector<glm::vec3> OB0_N;
-	std::vector<glm::vec3> OB1_N;
-	for (auto& jj : Ob0_Segs)
+	std::vector<glm::vec3> Ob0_N = { glm::normalize(Ob0_Segs[1] - Ob0_Segs[0]),
+		glm::normalize(Ob0_Segs[3] - Ob0_Segs[0]) ,
+		glm::normalize(Ob0_Segs[4] - Ob0_Segs[0]) };
+	std::vector<glm::vec3> Ob1_N = { glm::normalize(Ob1_Segs[1] - Ob1_Segs[0]),
+		glm::normalize(Ob1_Segs[3] - Ob1_Segs[0]) ,
+		glm::normalize(Ob1_Segs[4] - Ob1_Segs[0]) };
+	for (auto& jj : Ob0_N)
 	{
 		if (MATH::ProjColl(jj, Ob0_Segs, Ob1_Segs))
 		{
 			return false;
 		}
 	}
-	for (auto& jj : Ob1_Segs)
+	for (auto& jj : Ob1_N)
 	{
 		if (MATH::ProjColl(jj, Ob0_Segs, Ob1_Segs))
 		{
