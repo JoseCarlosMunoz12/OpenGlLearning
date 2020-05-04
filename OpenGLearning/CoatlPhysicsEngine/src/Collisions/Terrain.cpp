6c5135d42d8cf8dcd14b351d8de99aeaa807699f
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-05-03 21:35:45 -0500

    making Terrain Class to hold Bodies of Triangles

--- OpenGLearning/CoatlPhysicsEngine/src/Collisions/Terrain.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Collisions/Terrain.cpp
@@ -1,1 +1,22 @@
 #include "Terrain.h"
+using namespace CoatlPhysicsEngine;
+
+Terrain::Terrain(std::vector<glm::vec3> Mesh, std::vector<int> Index,int Dim)
+{
+	int Size = Index.size();
+	int Range = Size / 3 * 2;
+	this->TerWorld = std::make_unique<OctoTree>(glm::vec3(0.f),(float)Dim);
+	for (int ii = 0; ii < Range; ii++)
+	{
+
+	}
+}
+
+Terrain::~Terrain()
+{
+}
+
+std::vector<std::shared_ptr<Bodies>> Terrain::GetTerrs(glm::vec3 Pos, float Ext)
+{
+	return std::vector<std::shared_ptr<Bodies>>();
+}
--- OpenGLearning/CoatlPhysicsEngine/src/Collisions/Terrain.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Collisions/Terrain.h
@@ -1,6 +1,15 @@
 #pragma once
-
-class Terrain
+#include "../OptimizaAlgorithm/OctoTree.h"
+namespace CoatlPhysicsEngine
 {
-};
+	class Terrain
+	{
+	private:
+		std::unique_ptr<OctoTree> TerWorld;
+	public:
+		Terrain(std::vector<glm::vec3> Mesh, std::vector<int> Index,int Dim);
+		~Terrain();
+		std::vector<std::shared_ptr<Bodies>> GetTerrs(glm::vec3 Pos, float Ext);
+	};
 
+}
