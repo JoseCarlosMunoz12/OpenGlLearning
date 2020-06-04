b19a06856c087e5276933bbb52f4f9f134c86553
josecarlos munoz <josecarlos.munoz12@outlook.com>
2020-06-04 12:58:05 -0500

    finished adding GJK for Capsule and AABB

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/CapsuleRelAABB.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/CapsuleRelAABB.cpp
@@ -11,6 +11,19 @@ int CapsuleRelAABB::line_support(glm::vec3& Support, glm::vec3 D, Capsule Cap)
 	Support = AB[1];
 	return 0;
 }
+int CoatlPhysicsEngine::CapsuleRelAABB::poly_support(glm::vec3& Support, glm::vec3 D, AABB_Obj obj)
+{
+	int IMax = 0;
+	std::vector<glm::vec3> Vecs = obj.GetSegs();
+	float dMax = glm::dot(Vecs[0], D);
+	for (auto& j : Vecs)
+	{
+		float dot = glm::dot(j, D);
+		if (dot < dMax) continue;
+		IMax = 1, dMax = dot;
+	}
+	return IMax;
+}
 std::vector<std::shared_ptr<Contact>> CapsuleRelAABB::CapRelAABB(Capsule Cap, AABB_Obj Obj)
 {
 	std::shared_ptr<gjk_support> s = std::make_shared<gjk_support>();
@@ -20,14 +33,19 @@ std::vector<std::shared_ptr<Contact>> CapsuleRelAABB::CapRelAABB(Capsule Cap, AA
 	std::shared_ptr<gjk_simplex> gsx =std::make_shared<gjk_simplex>();
 	while (GJK(gsx,s))
 	{
+		s->AId = poly_support(s->A, s->DA, Obj);
 		s->BId = line_support(s->B, s->DB, Cap);
+		s->D = s->B - s->A;
 	}
+	std::shared_ptr<gjk_result> res = std::make_shared<gjk_result>();
+	GJK_analyze(res, gsx);
+	GJK_quat(res, 0, Cap.GetRadius());
 	std::vector<std::shared_ptr<Contact>> Temp;
 	std::shared_ptr<Contact> Cnt = std::make_shared<Contact>();
-	if (Dist != 0)
+	if (res->Dis != 0)
 	{
-		Cnt->Penetration;
-		Cnt->Normal;
+		Cnt->Penetration = res->Dis;
+		Cnt->Normal = glm::normalize(res->P1 - res->P1);
 	}
 	else
 	{
