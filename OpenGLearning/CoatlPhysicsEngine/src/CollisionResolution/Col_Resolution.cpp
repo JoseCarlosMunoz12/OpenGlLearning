eefc7f1dfd65a29d680ce42e1e739c417f79c7cc
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-06-20 22:32:34 -0500

    Redoing Collision Detection

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
@@ -1,75 +1,75 @@
 #include "Col_Resolution.h"
 using namespace CoatlPhysicsEngine;
-std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(Sphere Sph0, std::shared_ptr<Bodies> Bod0)
+std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(Sphere Sph0, std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1)
 {
-	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod0->GetShapes()))
+	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Sph0, *Sphere0);
 	}
-	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod0->GetShapes()))
+	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Sph0, *Cube0);
 	}
-	else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod0->GetShapes()))
+	else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Sph0, *Cap0);
 	}
-	//else if (std::shared_ptr<Triangles> Tr = std::dynamic_pointer_cast<Triangles>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<Triangles> Tr = std::dynamic_pointer_cast<Triangles>(Bod1->GetShapes()))
 	//{
-	//	return this->S_Res->GetContacts(*Tr, Bod1);
+	//	return this->S_Res->GetContacts(*Tr, Sph0);
 	//}
-	//else if (std::shared_ptr<OBB> Obj = std::dynamic_pointer_cast<OBB>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<OBB> Obj = std::dynamic_pointer_cast<OBB>(Bod1->GetShapes()))
 	//{
-	//	return this->S_Res->GetContacts(*Obj, Bod1);
+	//	return this->S_Res->GetContacts(*Obj, Sph0);
 	//}
 	return std::vector<std::shared_ptr<Contact>>();
 
 }
 
-std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(AABB_Obj Obj, std::shared_ptr<Bodies> Bod0)
+std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(AABB_Obj Obj, std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1)
 {
-	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod0->GetShapes()))
+	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Obj, *Sphere0);
 	}
-	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod0->GetShapes()))
+	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Obj, *Cube0);
 	}
-	else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod0->GetShapes()))
+	else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Obj, *Cap0);
 	}
-	//else if (std::shared_ptr<Triangles> Tr = std::dynamic_pointer_cast<Triangles>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<Triangles> Tr = std::dynamic_pointer_cast<Triangles>(Bod1->GetShapes()))
 	//{
 	//	return this->S_Res->GetContacts(*Tr, Bod1);
 	//}
-	//else if (std::shared_ptr<OBB> Obj = std::dynamic_pointer_cast<OBB>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<OBB> Obj = std::dynamic_pointer_cast<OBB>(Bod1->GetShapes()))
 	//{
 	//	return this->S_Res->GetContacts(*Obj, Bod1);
 	//}
 	return std::vector<std::shared_ptr<Contact>>();
 }
 
-std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(Capsule Cap, std::shared_ptr<Bodies> Bod0)
+std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(Capsule Cap, std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1)
 {
-	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod0->GetShapes()))
+	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Cap, *Sphere0);
 	}
-	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod0->GetShapes()))
+	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod1->GetShapes()))
 	{
 		return this->S_Res->GetContacts(Cap, *Cube0);
 	}
-	//else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod1->GetShapes()))
 	//{
 	//	return this->S_Res->GetContacts(Obj, *Cap0);
 	//}
-	//else if (std::shared_ptr<Triangles> Tr = std::dynamic_pointer_cast<Triangles>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<Triangles> Tr = std::dynamic_pointer_cast<Triangles>(Bod1->GetShapes()))
 	//{
 	//	return this->S_Res->GetContacts(*Tr, Bod1);
 	//}
-	//else if (std::shared_ptr<OBB> Obj = std::dynamic_pointer_cast<OBB>(Bod0->GetShapes()))
+	//else if (std::shared_ptr<OBB> Obj = std::dynamic_pointer_cast<OBB>(Bod1->GetShapes()))
 	//{
 	//	return this->S_Res->GetContacts(*Obj, Bod1);
 	//}
@@ -80,15 +80,15 @@ std::vector<std::shared_ptr<Contact>> Col_Resolution::MakeContacts(std::shared_p
 {
 	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod0->GetShapes()))
 	{
-		return this->MakeContacts(*Sphere0, Bod1);
+		return this->MakeContacts(*Sphere0, Bod0,Bod1);
 	}
 	else if (std::shared_ptr<AABB_Obj> Cube0 = std::dynamic_pointer_cast<AABB_Obj>(Bod0->GetShapes()))
 	{
-		return this->MakeContacts(*Cube0, Bod1);
+		return this->MakeContacts(*Cube0, Bod0, Bod1);
 	}
 	else if (std::shared_ptr<Capsule> Cap0 = std::dynamic_pointer_cast<Capsule>(Bod0->GetShapes()))
 	{
-		return this->MakeContacts(*Cap0, Bod1);
+		return this->MakeContacts(*Cap0, Bod0, Bod1);
 	}
 	glm::vec3 Se;
 	if (!this->S->EPA_GJK(Bod0->GetShapes(), Bod1->GetShapes(), Se))
--- OpenGLearning/CoatlPhysicsEngine/src/Collisions/BaseCols.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Collisions/BaseCols.cpp
@@ -130,7 +130,7 @@ bool BaseCols::UpdateBodies(OBB Tr, std::shared_ptr<Bodies> Bod1)
 
 bool BaseCols::ColBods(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1)
 {
-	return S->GJK(Bod0->GetShapes(), Bod1->GetShapes());
+	//return S->GJK(Bod0->GetShapes(), Bod1->GetShapes());
 	/*{
 		std::cout << "Collided\n";
 	}
@@ -138,7 +138,7 @@ bool BaseCols::ColBods(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod
 	{
 		std::cout << "No Collided\n";
 	}*/
-	/*if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod0->GetShapes()))
+	if (std::shared_ptr<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod0->GetShapes()))
 	{
 		return this->UpdateBodies(*Sphere0, Bod1);
 	}
@@ -158,7 +158,7 @@ bool BaseCols::ColBods(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod
 	{
 		return this->UpdateBodies(*Obj, Bod1);
 	}
-	return false;*/
+	return false;
 }
 
 BaseCols::BaseCols(std::string Name, std::shared_ptr<CollisionManager> InitCols)
