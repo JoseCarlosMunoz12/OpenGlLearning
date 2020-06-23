0f10ee170ef02df8c37d0f109e2e10c23cd1c80a
Jose Carlos Munoz <josecarlos.munoz12@outlook.com>
2020-06-23 15:08:51 -0500

    made class to handle SAT for shapes

---
+++ OpenGLearning/CoatlPhysicsEngine/src/SAT/SAT.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\type_ptr.hpp>
+#include <vector>
+#include "../Shapes/ColShapes.h"
+namespace CoatlPhysicsEngine {
+	class SAT
+	{
+	private:
+	public:
+		SAT();
+		~SAT();
+		glm::vec3 GetPenetrationContacts(std::shared_ptr<ColShapes> Bod0, std::shared_ptr<ColShapes> Bod1);
+	};
+}
