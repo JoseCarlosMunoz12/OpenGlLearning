50fa832baecf31c944caad0b7b5c9a23550b6c16
Jose Carlos Munoz <josecarlos.munoz12@outlook.com>
2020-06-24 14:48:23 -0500

    Capsule Rel can see if the capsule is parallel or perpendicular

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/CapsuleRelCapsule.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/CapsuleRelCapsule.cpp
@@ -5,7 +5,9 @@ std::vector<std::shared_ptr<Contact>> CapsuleRelCapsule::CapRel(Capsule Cap0, Ca
 {
 	glm::vec3 C_P0;
 	glm::vec3 C_P1;
-	MATH::ClosestSeg_Seg(Cap0.GetSegment(), Cap1.GetSegment(), C_P0, C_P1);
+	std::vector<glm::vec3> C_S0 = Cap0.GetSegment();
+	std::vector<glm::vec3> C_S1 = Cap1.GetSegment();
+	MATH::ClosestSeg_Seg(C_S0, C_S1, C_P0, C_P1);
 	std::vector<std::shared_ptr<Contact>> Temp;
 	std::shared_ptr<Contact> Con = std::make_shared<Contact>();
 	glm::vec3 Norm = C_P1 - C_P0;
@@ -21,7 +23,22 @@ std::vector<std::shared_ptr<Contact>> CapsuleRelCapsule::CapRel(Capsule Cap0, Ca
 	}
 	else
 	{
+		glm::vec3 C_N0 = C_S0[1] - C_S0[0];
+		glm::vec3 C_N1 = C_S1[1] - C_S1[0];
+		glm::vec3 N = glm::cross(C_N0, C_N1);
+		float Mag = glm::dot(N, N);
+		if (Mag == 0)
+		{
 
+		}
+		else if (Mag == 1.f)
+		{
+
+		}
+		else
+		{
+
+		}
 	}
 	return Temp;
 }
