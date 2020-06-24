725cc4a39633f2af5f456f8d97d25b18ac629bb4
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-06-23 21:06:17 -0500

    Need to get Normal from the shapes, if any

--- OpenGLearning/CoatlPhysicsEngine/src/SAT/SAT.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/SAT/SAT.cpp
@@ -1,14 +1,19 @@
 #include "SAT.h"
-
-CoatlPhysicsEngine::SAT::SAT()
+using namespace CoatlPhysicsEngine;
+SAT::SAT()
 {
 }
 
-CoatlPhysicsEngine::SAT::~SAT()
+SAT::~SAT()
 {
 }
 
-glm::vec3 CoatlPhysicsEngine::SAT::GetPenetrationContacts(std::shared_ptr<ColShapes> Bod0, std::shared_ptr<ColShapes> Bod1)
+glm::vec3 SAT::GetPenetrationContacts(std::shared_ptr<ColShapes> Bod0, std::shared_ptr<ColShapes> Bod1)
 {
+	std::vector<glm::vec3> Vertex0 = Bod0->GetVertices();
+	std::vector<glm::vec3> Vertex1 = Bod1->GetVertices();
+	std::vector<glm::vec3> Normals0;
+	std::vector<glm::vec3> Normals1;
+	
 	return glm::vec3();
 }
