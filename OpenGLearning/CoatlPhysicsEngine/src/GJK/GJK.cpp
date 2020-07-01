bd381cc5cdb8292f14b7e9b75cc3cf797dd84f28
JoseCarlos Munoz <josecarlos.munoz12@outlook.com>
2020-06-30 22:00:07 -0500

    Added EPA again and Now to create closeset face function

--- OpenGLearning/CoatlPhysicsEngine/src/GJK/GJK.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/GJK/GJK.cpp
@@ -24,9 +24,111 @@ glm::vec3 GJK_Alg::EPA_Support(std::shared_ptr<ColShapes> Shape0, std::shared_pt
 	return Shape0->EPA_Support(Dir) - Shape1->EPA_Support(-Dir);
 }
 
-glm::vec3 GJK_Alg::EPA(std::vector<glm::vec3> Verts, std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShapes> Shape1)
+glm::vec3 GJK_Alg::EPA(std::vector<glm::vec3> Vertex, std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShapes> Shape1)
 {
-	return glm::vec3();
+	glm::vec3 Faces[EPA_MAX_NUM_FACES][4];//Array of faces, each with 3 verts and a normal	
+
+	Faces[0][0] = Vertex[0];
+	Faces[0][1] = Vertex[1];
+	Faces[0][2] = Vertex[2];
+	Faces[0][3] = glm::normalize(glm::cross(Vertex[1] - Vertex[0], Vertex[2] - Vertex[0])); //ABC	
+	Faces[1][0] = Vertex[0];
+	Faces[1][1] = Vertex[2];
+	Faces[1][2] = Vertex[3];
+	Faces[1][3] = glm::normalize(glm::cross(Vertex[2] - Vertex[0], Vertex[3] - Vertex[0])); //ACD	
+	Faces[2][0] = Vertex[0];
+	Faces[2][1] = Vertex[3];
+	Faces[2][2] = Vertex[2];
+	Faces[2][3] = glm::normalize(glm::cross(Vertex[3] - Vertex[0], Vertex[1] - Vertex[0])); //ADB	
+	Faces[3][0] = Vertex[1];
+	Faces[3][1] = Vertex[3];
+	Faces[3][2] = Vertex[2];
+	Faces[3][3] = glm::normalize(glm::cross(Vertex[3] - Vertex[1], Vertex[2] - Vertex[1])); //BDC	
+	int Num_Face = 4;
+	int ClosestFace;
+	for (int It = 0; It < EPA_MAX_NUM_ITERATIONS; It++)
+	{
+		//Find Fce that's closest to origin	
+		float MinDist = glm::dot(Faces[0][0], Faces[0][3]);
+		ClosestFace = 0;
+		for (int ii = 1; ii < Num_Face; ii++)
+		{
+			float dist = glm::dot(Faces[ii][0], Faces[ii][3]);
+			if (dist < MinDist)
+			{
+				MinDist = dist;
+				ClosestFace = ii;
+			}
+		}
+		//search normal to face that's closets to origin	
+		glm::vec3 Search_Dir = Faces[ClosestFace][3];
+		glm::vec3 P = Shape1->EPA_Support(Search_Dir) - Shape0->EPA_Support(-Search_Dir);
+		float Dif = glm::dot(P, Search_Dir) - MinDist;
+		if (Dif < EPA_TOLERANCE)
+		{
+			float S = glm::dot(P, Search_Dir);
+			return Faces[ClosestFace][3] * S;
+		}
+		glm::vec3 LooseEdges[EPA_MAX_NUM_FACES][2];
+		int NumLooseEdge = 0;
+		//find all triangles that are facing P	
+		for (int ii = 0; ii < Num_Face; ii++)
+		{
+			if (glm::dot(Faces[ii][3], P - Faces[ii][0]) > 0)
+			{
+				for (int jj = 0; jj < 3; jj++)
+				{
+					glm::vec3 CurEdg[2] = { Faces[ii][jj],Faces[ii][(jj + 1) % 3] };
+					bool FoundEdge = false;
+					for (int kk = 0; kk < NumLooseEdge; kk++)
+					{
+						if (LooseEdges[kk][1] == CurEdg[0] && LooseEdges[kk][0] == CurEdg[1])
+						{
+							LooseEdges[kk][0] = LooseEdges[NumLooseEdge - 1][0];
+							LooseEdges[kk][1] = LooseEdges[NumLooseEdge - 1][1];
+							NumLooseEdge--;
+							FoundEdge = true;
+							kk = NumLooseEdge;
+						}
+					}
+					if (!FoundEdge)
+					{
+						if (NumLooseEdge >= EPA_MAX_NUM_LOOSE_EDGES)break;
+						LooseEdges[NumLooseEdge][0] = CurEdg[0];
+						LooseEdges[NumLooseEdge][1] = CurEdg[1];
+						NumLooseEdge++;
+					}
+				}
+				Faces[ii][0] = Faces[Num_Face - 1][0];
+				Faces[ii][1] = Faces[Num_Face - 1][1];
+				Faces[ii][2] = Faces[Num_Face - 1][2];
+				Faces[ii][3] = Faces[Num_Face - 1][3];
+				Num_Face--;
+				ii--;
+			}
+		}
+		//Reconstruct polytope with p added	
+		for (int ii = 0; ii < NumLooseEdge; ii++)
+		{
+			if (Num_Face >= EPA_MAX_NUM_FACES) break;
+			Faces[Num_Face][0] = LooseEdges[ii][0];
+			Faces[Num_Face][1] = LooseEdges[ii][1];
+			Faces[Num_Face][2] = P;
+			Faces[Num_Face][3] = glm::normalize(glm::cross(LooseEdges[ii][0] - LooseEdges[ii][1], LooseEdges[ii][0] - P));
+			float bias = 0.000001f;
+			float Diff = glm::dot(Faces[Num_Face][0], Faces[Num_Face][3]) + bias;
+			if (Dif < 0)
+			{
+				glm::vec3 Temp = Faces[Num_Face][0];
+				Faces[Num_Face][0] = Faces[Num_Face][1];
+				Faces[Num_Face][1] = Temp;
+				Faces[Num_Face][3] = -Faces[Num_Face][3];
+			}
+			Num_Face++;
+		}
+	}
+	float G = glm::dot(Faces[ClosestFace][0], Faces[ClosestFace][3]);
+	return Faces[ClosestFace][3] * G;
 }
 
 glm::vec3 GJK_Alg::C_F_E(std::vector<glm::vec3> Verts, std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShapes> Shape1)
@@ -221,7 +323,7 @@ bool GJK_Alg::EPA_GJK(std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShap
 	Verts.push_back(EPA_Support(Shape0, Shape1, Dir));
 	Simplex_Maker(Shape0, Shape1, Verts, Dir);
 	bool Col = false;
-	for (int ii = 0; ii < 64; ii++)
+	for (int ii = 0; ii < 124; ii++)
 	{
 		glm::vec3 A = EPA_Support(Shape0, Shape1, Dir);
 		if (glm::dot(A, Dir) < 0.f)
