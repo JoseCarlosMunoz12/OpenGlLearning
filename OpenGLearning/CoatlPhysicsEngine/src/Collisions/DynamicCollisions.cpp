9d11525802759d4222f48807741dca52d2ae7d2f
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-07-15 12:23:34 -0500

    improved on the gravity bunce of an object'

--- OpenGLearning/CoatlPhysicsEngine/src/Collisions/DynamicCollisions.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Collisions/DynamicCollisions.cpp
@@ -116,13 +116,16 @@ void DynamicCollisions::CheckCollision(std::shared_ptr<StaticCollisions> Statics
 							if (!this->ContainsManifold(ColRel, T))
 								ColRel.push_back(T);
 							Temp->AcumForce(-Gravity * Temp->GetMass());
-							if (glm::abs(Bod_Vel.z) > 0.125f)
+							if (Bod_Vel.z < 0)
 							{
-								Temp->SetVel(glm::vec3(Bod_Vel.x, Bod_Vel.y,-Bod_Vel.z/2));
-							}
-							else
-							{
-								Temp->SetVel(glm::vec3(Bod_Vel.x, Bod_Vel.y, 0.f));
+								if (glm::abs(Bod_Vel.z) > 0.125f)
+								{
+									Temp->SetVel(glm::vec3(Bod_Vel.x, Bod_Vel.y,glm::abs(Bod_Vel.z/2)));
+								}
+								else
+								{
+									Temp->SetVel(glm::vec3(Bod_Vel.x, Bod_Vel.y, 0.f));
+								}
 							}
 							jj->SetPosition(PrevPos);
 						}
