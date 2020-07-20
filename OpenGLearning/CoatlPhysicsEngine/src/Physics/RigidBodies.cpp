937bb0aaecb994302c0ad870321e0a88ec177648
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-07-20 11:15:06 -0500

    Made General information of the Rigid Bodies

--- OpenGLearning/CoatlPhysicsEngine/src/Physics/RigidBodies.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Physics/RigidBodies.cpp
@@ -2,18 +2,26 @@
 using namespace CoatlPhysicsEngine;
 
 RigidBodies::RigidBodies(glm::vec3 Pos)
-	:Particle(Pos)
+	:Particle(Pos),
+	RotDamp(1),RotVel(glm::vec3(0.f)),TorqueAcum(glm::vec3(0.f))
 {
 }
 
 RigidBodies::RigidBodies(glm::vec3 Pos, glm::vec3 InitVel)
-	:Particle(Pos, InitVel)
+	:Particle(Pos, InitVel),
+	RotDamp(1), RotVel(glm::vec3(0.f)), TorqueAcum(glm::vec3(0.f))
 {
 }
 
 RigidBodies::RigidBodies(glm::vec3 Pos, glm::vec3 InitVel, float InitDamp)
-	:Particle(Pos, InitVel,InitDamp)
+	:Particle(Pos, InitVel,InitDamp),
+	RotDamp(1), RotVel(glm::vec3(0.f)), TorqueAcum(glm::vec3(0.f))
 {
+}
+
+void RigidBodies::SetInertia(glm::mat4 InitInitertia)
+{
+	this->Intertia = InitInitertia;
 }
 
 RigidBodies::~RigidBodies()
--- OpenGLearning/CoatlPhysicsEngine/src/Physics/RigidBodies.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Physics/RigidBodies.h
@@ -4,10 +4,15 @@ namespace CoatlPhysicsEngine {
 	class RigidBodies : public Particle
 	{
 	private:
+		glm::mat4 Intertia;
+		glm::vec3 RotVel;
+		glm::vec3 TorqueAcum;
+		float RotDamp;
 	public:
 		RigidBodies(glm::vec3 Pos);
 		RigidBodies(glm::vec3 Pos, glm::vec3 InitVel);
 		RigidBodies(glm::vec3 Pos, glm::vec3 InitVel, float InitDamp);
+		void SetInertia(glm::mat4 InitInitertia);
 		~RigidBodies();
 	};	
 }
