83774c386213302b3b03aac5db4028970a895a6c
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-07-23 13:17:46 -0500

    Made inits for the base class for Part and RigBod

--- OpenGLearning/CoatlPhysicsEngine/src/Physics/Bod_Base.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Physics/Bod_Base.cpp
@@ -1,1 +1,30 @@
 #include "Bod_Base.h"
+using namespace CoatlPhysicsEngine;
+
+Bod_Base::Bod_Base(glm::vec3 InitPos)
+	:Pos(InitPos),AccumForce(glm::vec3(0.f)),Vel(glm::vec3(0.f)), LinDamp(1.f), InvMass(0.f)
+{
+}
+
+Bod_Base::Bod_Base(glm::vec3 InitPos, glm::vec3 InitVel)
+	: Pos(InitPos), AccumForce(glm::vec3(0.f)), Vel(InitVel),
+	LinDamp(1.f), InvMass(0.f)
+{
+}
+
+Bod_Base::~Bod_Base()
+{
+}
+
+void Bod_Base::SetMass(float Mass)
+{
+	if (Mass != 0.f)
+		this->InvMass = 1 / Mass;
+	else
+		this->InvMass = 0.f;
+}
+
+void Bod_Base::SetVel(glm::vec3 SetVel)
+{
+	this->Vel = SetVel;
+}
--- OpenGLearning/CoatlPhysicsEngine/src/Physics/Bod_Base.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Physics/Bod_Base.h
@@ -1,7 +1,28 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\type_ptr.hpp>
+#include <vector>
 namespace CoatlPhysicsEngine {
 	class Bod_Base
 	{
+	protected:
+		glm::vec3 Pos;
+		glm::vec3 AccumForce;
+		glm::vec3 Vel;
+		float LinDamp;
+		float InvMass;
+	public:
+		Bod_Base(glm::vec3 Pos);
+		Bod_Base(glm::vec3 InitPos, glm::vec3 InitVel);
+		~Bod_Base();
+		void SetMass(float Mass);
+		void SetVel(glm::vec3 SetVel);
 	};
 }
 
