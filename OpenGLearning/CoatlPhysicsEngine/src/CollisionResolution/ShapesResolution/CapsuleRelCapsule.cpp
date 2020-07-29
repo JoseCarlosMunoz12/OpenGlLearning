1fd5e91dbdc9d8d75be63ff0c9d019449fe90bf6
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-07-29 06:56:41 -0500

    Able to create Points of Capsule capsule collision resolution

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/CapsuleRelCapsule.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/CapsuleRelCapsule.cpp
@@ -17,7 +17,6 @@ std::vector<std::shared_ptr<Contact>> CapsuleRelCapsule::CapRel(Capsule Cap0, Ca
 	std::vector<glm::vec3> Cont;
 	std::vector<glm::vec3> PL0;
 	std::vector<glm::vec3> PL1;
-	std::vector<glm::vec3> LinCont;
 	if (Norm != glm::vec3(0.f))
 	{
 		float S = glm::dot(Norm, Dir0);
@@ -27,11 +26,17 @@ std::vector<std::shared_ptr<Contact>> CapsuleRelCapsule::CapRel(Capsule Cap0, Ca
 			Cont = MATH::SAT_Points(Dir0, C_S0, C_S1);
 			PL0 = MATH::SAT_Points(Norm, C_S0, C_S1);
 			PL1 = MATH::SAT_Points(glm::cross(Dir0, Norm), C_S0, C_S1);
+			float Total_R = Cap0.GetRadius() + Cap1.GetRadius();
+			float dis = glm::distance(C_P0, C_P1);
 			for (auto& ii : Cont)
 			{
+				std::shared_ptr<Contact> Contacts = std::make_shared<Contact>();
 				glm::vec3 R =( PL0[0] + PL0[1]) / 2.f;
 				glm::vec3 T = (PL1[0] + PL1[1]) / 2.f;
-				LinCont.push_back(ii + R +T);
+				Contacts->ContactPoint = ii + R +T;
+				Contacts->Normal = -Norm;
+				Contacts->Penetration = Total_R - dis;
+				Temp.push_back(Contacts);
 			}
 		}
 		else
@@ -39,7 +44,7 @@ std::vector<std::shared_ptr<Contact>> CapsuleRelCapsule::CapRel(Capsule Cap0, Ca
 			float Total_R = Cap0.GetRadius() + Cap1.GetRadius();
 			float dis = glm::distance(C_P0, C_P1);
 			Con->Penetration = Total_R - dis;
-			Con->Normal = Norm;
+			Con->Normal = -Norm;
 			Con->ContactPoint = C_P0 - Norm * Con->Penetration;
 			Temp.push_back(Con);
 		}
