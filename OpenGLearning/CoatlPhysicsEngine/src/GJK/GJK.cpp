8ae41b45ae65ef5d0ed977526a0e6fef738a8e26
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-10 08:30:02 -0500

    Seg's Mids
    Obtain midway of Segments to be able to calculte init Dir

--- OpenGLearning/CoatlPhysicsEngine/src/GJK/GJK.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/GJK/GJK.cpp
@@ -63,6 +63,153 @@ glm::vec3 GJK_Alg::Support(std::vector<glm::vec3> Seg, glm::vec3 Dir)
 	return MaxPnt;
 }
 
+bool GJK_Alg::Simplex_Maker(glm::vec3 Pos0, glm::vec3 Pos1,
+	std::vector<glm::vec3>& Verts, glm::vec3& Dir)
+{
+	int Size = Verts.size();
+	switch (Size)
+	{
+	case 0: {
+		Dir = Pos0 - Pos1;
+	}break;
+	case 1: {
+		Dir = -Verts[0];
+	}break;
+	case 2: {
+		glm::vec3 AB = Verts[0] - Verts[1];
+		glm::vec3 A0 = -Verts[1];
+		if (glm::dot(AB, A0) > 0.f)
+			Dir = TripleCross(AB, A0);
+		else
+			Dir = A0;
+	}break;
+	case 3: {
+		glm::vec3 A = Verts[2];
+		glm::vec3 B = Verts[1];
+		glm::vec3 C = Verts[0];
+
+		glm::vec3 AB = B - A;
+		glm::vec3 AC = C - A;
+		glm::vec3 A0 = -A;
+		glm::vec3 ABC = glm::cross(AB, AC);
+
+		glm::vec3 ABC_AC = glm::cross(ABC, AC);
+		glm::vec3 AB_ABC = glm::cross(AB, ABC);
+
+		if (glm::dot(ABC_AC, A0) > 0.f)
+		{
+			if (glm::dot(AC, A0) > 0.f)
+				Dir = TripleCross(AC, A0);
+			else
+			{
+				if (glm::dot(AB, A0) > 0.f)
+					Dir = TripleCross(AB, A0);
+				else
+					Dir = A0;
+			}
+		}
+		else
+		{
+			if (glm::dot(AB_ABC, A0) > 0.f)
+			{
+				if (glm::dot(AB, A0) > 0.f)
+					Dir = TripleCross(AB, A0);
+				else
+					Dir = A0;
+			}
+			else
+			{
+				if (glm::dot(ABC, A0) > 0.f)
+					Dir = ABC;
+				else
+					Dir = -ABC;
+			}
+		}
+	}break;
+	case 4: {
+		glm::vec3 A = Verts[3];
+		glm::vec3 B = Verts[2];
+		glm::vec3 C = Verts[1];
+		glm::vec3 D = Verts[0];
+
+		glm::vec3 AC = C - A;
+		glm::vec3 AD = D - A;
+		glm::vec3 AB = B - A;
+		glm::vec3 A0 = -A;
+
+		glm::vec3 AC_AB = glm::cross(AC, AB);
+		glm::vec3 AB_AD = glm::cross(AB, AD);
+		glm::vec3 AD_AC = glm::cross(AD, AC);
+
+		float vABC = glm::dot(AC_AB, A0);
+		float vABD = glm::dot(AB_AD, A0);
+		float vADC = glm::dot(AD_AC, A0);
+
+		int neg = 0;
+		int pos = 0;
+
+		if (vABC >= 0)
+			pos++;
+		else
+			neg++;
+
+		if (vABD > 0)
+			pos++;
+		else
+			neg++;
+
+		if (vADC > 0)
+			pos++;
+		else
+			neg++;
+
+		if (neg == 3)
+			return true;
+		if (pos == 3)
+			return true;
+		if (neg == 2 and pos == 1)
+		{
+			if (vADC > 0.f)
+			{
+				Verts.erase(Verts.begin() + 1);
+				Dir = AD_AC;
+			}
+			else if (vABD > 0.f)
+			{
+				Verts.erase(Verts.begin() + 2);
+				Dir = AB_AD;
+			}
+			else
+			{
+				Verts.erase(Verts.begin() + 0);
+				Dir = AC_AB;
+			}
+		}
+		else
+		{
+			if (vADC < 0.f)
+			{
+				Verts.erase(Verts.begin() + 1);
+				Dir = -AD_AC;
+			}
+			else if (vABD < 0.f)
+			{
+				Verts.erase(Verts.begin() + 2);
+				Dir = -AB_AD;
+			}
+			else
+			{
+				Verts.erase(Verts.begin() + 0);
+				Dir = -AC_AB;
+			}
+		}
+	}break;
+	default:
+		break;
+	}
+	return false;
+}
+
 glm::vec3 GJK_Alg::EPA_Support(std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShapes> Shape1, glm::vec3 Dir)
 {
 	return Shape0->EPA_Support(Dir) - Shape1->EPA_Support(-Dir);
@@ -483,10 +630,12 @@ bool GJK_Alg::GJK(std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShapes> 
 	std::vector<glm::vec3> Seg0, std::vector<glm::vec3> Seg1)
 {
 	std::vector<glm::vec3> Verts;
+	glm::vec3 Pos0 = (Seg0[1] + Seg0[0]) / 2.f;
+	glm::vec3 Pos1 = (Seg1[1] + Seg1[0]) / 2.f;
 	glm::vec3 Dir;
-	Simplex_Maker(Shape0, Shape1, Verts, Dir);
+	Simplex_Maker(Pos0, Pos1, Verts, Dir);
 	Verts.push_back(Support(Shape0, Shape1, Dir));
-	Simplex_Maker(Shape0, Shape1, Verts, Dir);
+	Simplex_Maker(Pos0, Pos1, Verts, Dir);
 	for (int ii = 0; ii < 20; ii++)
 	{
 		glm::vec3 A = Support(Shape0, Shape1, Dir);
@@ -495,7 +644,7 @@ bool GJK_Alg::GJK(std::shared_ptr<ColShapes> Shape0, std::shared_ptr<ColShapes> 
 		if (std::find(Verts.begin(), Verts.end(), A) != Verts.end())
 			break;
 		Verts.push_back(A);
-		if (Simplex_Maker(Shape0, Shape1, Verts, Dir))
+		if (Simplex_Maker(Pos0, Pos1, Verts, Dir))
 			return true;
 	}
 	return false;
