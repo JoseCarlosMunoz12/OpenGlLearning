28628dea37a14c7373b6246ce544bfbb22e859ca
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-10 07:44:00 -0500

    GJK for All Shaps
    GJK will be used to calculate the  sweep test

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
@@ -165,7 +165,7 @@ void Col_Resolution::ResolveContacts(std::shared_ptr<Manifold> Cnt)
 	//(false;Cnt->Contacts[0]->Normal; Cnt->Contacts[0]->Penetration;
 	if (Cnt->ContactCount > 0)
 	{
-		float Diff = 0.f;
+		float Diff = Cnt->Contacts[0]->Penetration;
 		if (Diff < 0.001)
 			Diff = 0.f;
 		glm::vec3 Norm = Cnt->Contacts[0]->Normal;
