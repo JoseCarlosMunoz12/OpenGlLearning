6c93b3361235c6ed72d885747c178b997f0f97a0
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-11 14:31:14 -0500

    Finished base
    Finished the base items for joints that will be used

--- OpenGLearning/CoatlPhysicsEngine/src/Bodies/Joints/Joints.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Bodies/Joints/Joints.h
@@ -1,8 +1,12 @@
 #pragma once
+#include "../BodyParts.h"
 namespace CoatlPhysicsEngine
 {
 	class ColJoints
 	{
+	protected:
+		std::weak_ptr<BodyParts> BodA;
+		std::weak_ptr<BodyParts> BodB;
 	public:
 		ColJoints() {};
 		~ColJoints() {};
