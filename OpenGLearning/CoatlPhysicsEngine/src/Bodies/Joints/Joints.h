f9a969b71925fde440345ae57a3656daf413da6f
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-12 08:02:10 -0500

    Edits of joints
    able to get parameters and edit them

--- OpenGLearning/CoatlPhysicsEngine/src/Bodies/Joints/Joints.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Bodies/Joints/Joints.h
@@ -13,7 +13,14 @@ namespace CoatlPhysicsEngine
 		ColJoints() {};
 		~ColJoints() {};
 		virtual void SetBodies(std::weak_ptr<BodyParts> NewBodA, std::weak_ptr<BodyParts> NewBodB,
-			float Max, float Min = 1.f, glm::quat Angle = glm::quat(1.f, 0.f, 0.f, 0.f)){ return; };
-		virtual std::shared_ptr<Manifold> GetResolutin() { return std::shared_ptr<Manifold>(); };
+			float Max, float Min = 1.f, glm::quat Angle = glm::quat(1.f, 0.f, 0.f, 0.f)) { return; };
+		virtual std::shared_ptr<Manifold> GetResolution() { return std::shared_ptr<Manifold>(); };
+		//Sets parameters
+		virtual void SetMin(float NewMin) { return; };
+		virtual void SetMax(float NewMax) { return; };
+		virtual void SetAngle(glm::quat NewAngle) { return; };
+		//Get Parameters
+		virtual float GetMax() { return this->Max; };
+
 	};
 }
