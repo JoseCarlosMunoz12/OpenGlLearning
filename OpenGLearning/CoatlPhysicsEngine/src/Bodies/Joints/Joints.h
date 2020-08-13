314629aff2b3ceed7d974824f5b343b3ff52c0ba
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-13 07:25:09 -0500

    simplifying
    simplifying the setting of joints

--- OpenGLearning/CoatlPhysicsEngine/src/Bodies/Joints/Joints.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Bodies/Joints/Joints.h
@@ -11,8 +11,7 @@ namespace CoatlPhysicsEngine
 	public:
 		ColJoints():Max(1.f) {};
 		~ColJoints() {};
-		virtual void SetBodies(std::weak_ptr<BodyParts> NewBodA, std::weak_ptr<BodyParts> NewBodB,
-			float Max, float Min = 1.f, glm::quat Angle = glm::quat(1.f, 0.f, 0.f, 0.f)) { return; };
+		virtual void SetBodies(std::weak_ptr<BodyParts> NewBodA, std::weak_ptr<BodyParts> NewBodB) { return; };
 		//Sets parameters
 		virtual void SetMin(float NewMin) { return; };
 		virtual void SetMax(float NewMax) { return; };
