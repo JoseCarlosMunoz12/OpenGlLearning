4c87a2dd22fce91d362261b2d64b0088d1bc502c
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-13 08:46:33 -0500

    Get and Set
    Able to delete a specific joints, and add and get whole list

--- OpenGLearning/CoatlPhysicsEngine/src/Bodies/Bod_Collection.h
+++ OpenGLearning/CoatlPhysicsEngine/src/Bodies/Bod_Collection.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Joints/Joints.h"
+#include "Bodies.h"
 namespace CoatlPhysicsEngine {
 	class Collection
 	{
@@ -7,7 +8,8 @@ namespace CoatlPhysicsEngine {
 	public:
 		Collection();
 		~Collection();
-		void MakeJoint();
+		void MakeJoint(std::shared_ptr<ColJoints> NewCol);
 		void DeleteJoint(int ID);
+		std::vector<std::shared_ptr<ColJoints>> GetJoints();
 	};	
 }
