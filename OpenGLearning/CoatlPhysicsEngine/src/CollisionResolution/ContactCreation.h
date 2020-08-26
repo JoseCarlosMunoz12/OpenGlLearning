53fa5b5c1b377be8b3fceddf30c8c9ac9f684a55
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-26 08:58:58 -0500

    Corrected Null
    made sure to test if the body is an empty physics body

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ContactCreation.h
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ContactCreation.h
@@ -16,6 +16,8 @@ namespace CoatlPhysicsEngine
 		std::vector<std::shared_ptr<Contacts>> MakeContacts(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 		std::unique_ptr<GJK_Alg> GJK_;
 		std::unique_ptr<SAT> SAT_;
+		float GetLowestFric(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
+		float GetLowestRest(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 	public:
 		ContactCreation();
 		~ContactCreation();
