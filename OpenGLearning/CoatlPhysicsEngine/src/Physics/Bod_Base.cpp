44319bfc6297dcf9b4d6c9d72448b6bee1eb468e
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-08-28 10:59:22 -0500

    Angular damp removed
    There is no angular dampining for rigid bodies

--- OpenGLearning/CoatlPhysicsEngine/src/Physics/Bod_Base.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/Physics/Bod_Base.cpp
@@ -3,14 +3,14 @@ using namespace CoatlPhysicsEngine;
 
 Bod_Base::Bod_Base(glm::vec3 InitPos)
 	:Pos(InitPos),AccumForce(glm::vec3(0.f)),Vel(glm::vec3(0.f)), LinDamp(0.8f), InvMass(0.f)
-	, RotVel(glm::vec3(0.f)), RotDamp(.8f),Motion(0.f),PrevAccel(glm::vec3(0.f)), Friction(10.f), Restitution(0.4f)
+	, RotVel(glm::vec3(0.f)), RotDamp(1.f),Motion(0.f),PrevAccel(glm::vec3(0.f)), Friction(10.f), Restitution(0.4f)
 {
 	this->AxisAngle = glm::angleAxis(0.f, glm::vec3(1.f, 0.f, 0.f));
 }
 
 Bod_Base::Bod_Base(glm::vec3 InitPos, glm::vec3 InitVel)
 	: Pos(InitPos), AccumForce(glm::vec3(0.f)), Vel(InitVel),
-	LinDamp(.8f), InvMass(0.f),RotVel(glm::vec3(0.f)),RotDamp(1.f)
+	LinDamp(.8f), InvMass(0.f),RotVel(glm::vec3(0.f)),RotDamp(1.f)	
 	, Motion(0.f), PrevAccel(glm::vec3(0.f)),Friction(10.f), Restitution(0.4f)
 {
 	this->AxisAngle = glm::angleAxis(0.f, glm::vec3(1.f, 0.f, 0.f));
@@ -18,7 +18,7 @@ Bod_Base::Bod_Base(glm::vec3 InitPos, glm::vec3 InitVel)
 
 Bod_Base::Bod_Base(glm::vec3 InitPos, glm::vec3 InitVel, float InitDamp)
 	: Pos(InitPos), AccumForce(glm::vec3(0.f)), Vel(InitVel),
-	LinDamp(InitDamp), InvMass(0.f), RotVel(glm::vec3(.8f)), RotDamp(1.f)
+	LinDamp(InitDamp), InvMass(0.f), RotVel(glm::vec3(1.f)), RotDamp(1.f)
 	, Motion(0.f), PrevAccel(glm::vec3(0.f)), Friction(10.f), Restitution(0.4f)
 {
 	this->AxisAngle = glm::angleAxis(0.f, glm::vec3(1.f, 0.f, 0.f));
