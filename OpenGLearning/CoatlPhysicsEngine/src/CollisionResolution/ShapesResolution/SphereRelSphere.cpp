e04577d3b1f9461db8549d23b7ed7e4087440079
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-09-22 07:15:51 -0500

    Sphere is Updated
    The sphere is updated to get the R from center to contact point

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/SphereRelSphere.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ShapesResolution/SphereRelSphere.cpp
@@ -7,19 +7,16 @@ std::vector<std::shared_ptr<Contacts>> SphereRelSphere::SphRelSph(Sphere Sph0, S
 	float TotalR = Sph0.GetRadius() + Sph1.GetRadius();
 	glm::vec3 MidLine = Sph1.GetPos() - Sph0.GetPos();
 	float Size = glm::length(MidLine);
-	glm::vec3 Norm;
+	glm::vec3 Norm = glm::vec3(1.f, 0.f, 0.f);
 	if (Size == 0.f)
-	{
-		Norm = glm::vec3(1.f, 0.f, 0.f);
 		Size = TotalR;
-	}
 	else
 		Norm = MidLine / Size;
-	Temp->Normal = -Norm;
+	Temp->Normal = Norm;
 	Temp->Penetration = TotalR - Size;
 	Temp->ContactPoint.push_back(Sph1.GetPos() - MidLine * .5f);
-	Temp->R0.push_back(Sph1.GetPos() + MidLine);
-	Temp->R1.push_back(Sph0.GetPos() - MidLine);
+	Temp->R0.push_back(Norm * Sph0.GetRadius());
+	Temp->R1.push_back(-Norm * Sph1.GetRadius());
 	Contact.push_back(Temp);
 	return Contact;
 }
