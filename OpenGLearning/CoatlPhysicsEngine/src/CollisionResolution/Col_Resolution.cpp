6008d48e95f9ebe801b09be2889549eacebf5918
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-09-24 07:57:59 -0500

    renamed
    renamed a variable for easier understandng

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
@@ -21,8 +21,8 @@ void Col_Resolution::ResolveContacts(std::vector<std::shared_ptr<Contacts>> Cnt,
 			glm::vec3 Vec = Cnt[0]->Bods[0]->GetParticle()->GetVel();
 			glm::vec3 Pos0 = Cnt[0]->Bods[0]->GetPos();
 			glm::vec3 Pos1 = Cnt[0]->Bods[1]->GetPos();
-			glm::vec3 Dif = (Cnt[0]->R1[0] + Pos1) - (Cnt[0]->R0[0] + Pos0);
-			float CN =  glm::dot(Dif, Cnt[0]->Normal);
+			glm::vec3 C = (Cnt[0]->R1[0] + Pos1) - (Cnt[0]->R0[0] + Pos0);
+			float CN =  glm::dot(C, Cnt[0]->Normal);
 			Vec.z = (.3f / dt) * CN;
 			Cnt[0]->Bods[0]->GetParticle()->SetVel(Vec);
 			Cnt[0]->Bods[0]->GetParticle()->SetPos(Pos0 + Vec * dt);
