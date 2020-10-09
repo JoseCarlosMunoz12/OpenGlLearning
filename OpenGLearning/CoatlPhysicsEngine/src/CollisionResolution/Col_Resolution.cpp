f5d5893b790bc9a9cc9bf9b195aa3c62f3499141
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-10-09 13:03:33 -0500

    Quick Fix
    made a quick fix for the K value when multilpying with b

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
@@ -23,13 +23,22 @@ void Col_Resolution::ResolveContacts(std::vector<std::shared_ptr<Contacts>> Cnt,
 		glm::quat E(0,AngVel * jj->dt0);
 		Quat = Quat + .5f * Quat * E;
 		glm::vec3 Pos1 = Cnt[0]->Bods[1]->GetPos();
+		//Calculate values of Lambda, C and b
 		glm::vec3 C = (jj->R1[0] + Pos1) - (jj->R0[0] + Pos0);
 		float CN = glm::dot(C, jj->Normal);
 		float B = 0.3f;
 		float b = jj->Restituion * glm::dot(-Vec0 - glm::cross(AngVel,jj->R0[0]), jj->Normal) + (B / jj->dt1) * CN;
-		float lambda = (Mass0 + glm::dot(glm::cross(jj->R0[0], jj->Normal) * InvInert, glm::cross(jj->R0[0],jj->Normal))) * b;
+		float lambda = glm::pow(Mass0 + glm::dot(glm::cross(jj->R0[0], jj->Normal) * InvInert, glm::cross(jj->R0[0],jj->Normal)),-1) * b;
+		//Calculate new Vel and AngVel
+		Vec0 += -1.f * Mass0 * jj->Normal * lambda;
+		AngVel +=  InvInert * glm::cross(jj->R0[0], jj->Normal) * -1.f * lambda;
+		//Setting new pos  and ang
 		jj->Bods[0]->GetParticle()->SetVel(Vec0);
+		jj->Bods[0]->GetParticle()->SetRotVel(AngVel);
+		glm::quat E(0, AngVel * jj->dt1);
+		Quat = Quat + .5f * Quat * E;
 		Pos0 += jj->dt1 * Vec0;
+		jj->Bods[0]->SetQuat(Quat);
 		jj->Bods[0]->SetPosition(Pos0);
 	}
 }
