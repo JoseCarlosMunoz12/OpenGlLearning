7669ebce873f1d797c986c322e8e345f8ee1c262
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-10-09 12:51:32 -0500

    Adding Quat and Angular vel
    Adding angular velocities and JV for calculating new vel and ang

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
@@ -15,15 +15,19 @@ void Col_Resolution::ResolveContacts(std::vector<std::shared_ptr<Contacts>> Cnt,
 	for (auto& jj : Cnt)
 	{
 		glm::vec3 Vec0 = jj->Bods[0]->GetParticle()->GetVel();
+		float Mass0 = 1 / jj->Bods[0]->GetParticle()->GetMass();
+		glm::mat3 InvInert = glm::inverse(jj->Bods[0]->GetParticle()->GetInertiaWorld());
 		glm::vec3 Pos0 = jj->Bods[0]->GetPos() + Vec0 * jj->dt0;
 		glm::vec3 AngVel = jj->Bods[0]->GetParticle()->GetRotVel();
 		glm::quat Quat = jj->Bods[0]->GetQuat();
+		glm::quat E(0,AngVel * jj->dt0);
+		Quat = Quat + .5f * Quat * E;
 		glm::vec3 Pos1 = Cnt[0]->Bods[1]->GetPos();
 		glm::vec3 C = (jj->R1[0] + Pos1) - (jj->R0[0] + Pos0);
 		float CN = glm::dot(C, jj->Normal);
 		float B = 0.3f;
-		float R = jj->Restituion * glm::dot(-Vec0 - glm::cross(AngVel,jj->R0[0]), jj->Normal);
-		Vec0.z = R + (B / jj->dt1) * CN;
+		float b = jj->Restituion * glm::dot(-Vec0 - glm::cross(AngVel,jj->R0[0]), jj->Normal) + (B / jj->dt1) * CN;
+		float lambda = (Mass0 + glm::dot(glm::cross(jj->R0[0], jj->Normal) * InvInert, glm::cross(jj->R0[0],jj->Normal))) * b;
 		jj->Bods[0]->GetParticle()->SetVel(Vec0);
 		Pos0 += jj->dt1 * Vec0;
 		jj->Bods[0]->SetPosition(Pos0);
