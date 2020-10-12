7aa753593c00a147698c2eaa6585b814b6952f0a
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-10-11 22:33:22 -0500

    Fixed resolving vel
    fix the resolve of vel

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/Col_Resolution.cpp
@@ -22,6 +22,7 @@ void Col_Resolution::ResolveContacts(std::vector<std::shared_ptr<Contacts>> Cnt,
 		glm::quat Quat = jj->Bods[0]->GetQuat();
 		glm::quat E(0,AngVel * jj->dt0);
 		Quat = Quat + .5f * Quat * E;
+		Quat = glm::normalize(Quat);
 		glm::vec3 Pos1 = Cnt[0]->Bods[1]->GetPos();
 		//Calculate values of Lambda, C and b
 		glm::vec3 C = (jj->R1[0] + Pos1) - (jj->R0[0] + Pos0);
@@ -30,13 +31,14 @@ void Col_Resolution::ResolveContacts(std::vector<std::shared_ptr<Contacts>> Cnt,
 		float b = jj->Restituion * glm::dot(-Vec0 - glm::cross(AngVel,jj->R0[0]), jj->Normal) + (B / jj->dt1) * CN;
 		float lambda = glm::pow(Mass0 + glm::dot(glm::cross(jj->R0[0], jj->Normal) * InvInert, glm::cross(jj->R0[0],jj->Normal)),-1) * b;
 		//Calculate new Vel and AngVel
-		Vec0 += -1.f * Mass0 * jj->Normal * lambda;
+		Vec0 = Vec0 + Mass0 * jj->Normal * lambda;
 		AngVel +=  InvInert * glm::cross(jj->R0[0], jj->Normal) * -1.f * lambda;
 		//Setting new pos  and ang
 		jj->Bods[0]->GetParticle()->SetVel(Vec0);
 		jj->Bods[0]->GetParticle()->SetRotVel(AngVel);
-		glm::quat E(0, AngVel * jj->dt1);
+		E = glm::quat(0, AngVel * jj->dt1);
 		Quat = Quat + .5f * Quat * E;
+		Quat = glm::normalize(Quat);
 		Pos0 += jj->dt1 * Vec0;
 		jj->Bods[0]->SetQuat(Quat);
 		jj->Bods[0]->SetPosition(Pos0);
