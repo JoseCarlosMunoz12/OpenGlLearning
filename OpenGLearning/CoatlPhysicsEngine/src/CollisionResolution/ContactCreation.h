674537eb34cfbc00ad66a6d6bf4fd554e577409f
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2020-10-14 22:14:03 -0500

    Ready for constraint
    Need to make constraints for Contact and other constraints

--- OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ContactCreation.h
+++ OpenGLearning/CoatlPhysicsEngine/src/CollisionResolution/ContactCreation.h
@@ -9,13 +9,13 @@ namespace CoatlPhysicsEngine
 	class ContactCreation
 	{
 		//Contact creation
+		std::unique_ptr<ShapeResolution> S_Res;
 		std::vector<std::shared_ptr<Contacts>> ContactCreate(Sphere Sph0, std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 		std::vector<std::shared_ptr<Contacts>> ContactCreate(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
-		std::vector<std::shared_ptr<Contacts>> ContactCreate(Capsule Cap, std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
-		std::unique_ptr<ShapeResolution> S_Res;
-		std::vector<std::shared_ptr<Contacts>> MakeContacts(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 		std::unique_ptr<GJK_Alg> GJK_;
 		std::unique_ptr<SAT> SAT_;
+		std::vector<std::shared_ptr<Contacts>> ContactCreate(Capsule Cap, std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
+		std::vector<std::shared_ptr<Contacts>> MakeContacts(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 		float GetLowestFric(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 		float GetLowestRest(std::shared_ptr<Bodies> Bod0, std::shared_ptr<Bodies> Bod1);
 	public:
